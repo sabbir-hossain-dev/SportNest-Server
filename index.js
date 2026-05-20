443c3f96e6797640ac6d983d86b8ec23ec0db65f
Sabbir Hossain <sabbir5235@gmail.com>
2026-05-21 00:28:01 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true,
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Server Running ...");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
